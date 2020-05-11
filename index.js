d30ca3fd9794495026bbbf08c67e6f7c37a4488c
kevinjk27 <52363107+kevinjk27@users.noreply.github.com>
2020-05-10 20:30:11 -0700

    adds detail page of chosen car via MongoDB

--- index.js
+++ index.js
@@ -29,38 +29,51 @@ const Car = require("./models/cars");
 //Homepage using MongoDB Routes Integration
 app.get('/', (req, res, next) => {
   Car.find({}).lean()
-  .then((cars) => {
-    res.render('home', {cars}); 
-  })
-  .catch(err => next(err));
+    .then((cars) => {
+      res.render('home', { cars });
+    })
+    .catch(err => next(err));
 });
-
 
 
 // send plain text response
 app.get('/about', (req, res) => {
-    res.type('text/plain');
-    res.send('My name is Kevin. I am from Indonesia. This is my 5th quarter in Seattle Central to pursue AAS degree in Website Development. I\'m taking this course because in WEB150 I was only being taught about plain vanilla JS. As this language is really popular among the developers and the application is so extensive, I\'d like to learn more about Node, Vue, React Native and Express.');
+  res.type('text/plain');
+  res.send('My name is Kevin. I am from Indonesia. This is my 5th quarter in Seattle Central to pursue AAS degree in Website Development. I\'m taking this course because in WEB150 I was only being taught about plain vanilla JS. As this language is really popular among the developers and the application is so extensive, I\'d like to learn more about Node, Vue, React Native and Express.');
 });
 
-// send content of 'home' view using Express
-app.get('/detail', (req, res) => {
-    let detail = cars.getCar(req.query.make)
-    res.render('details', { make: req.query.make, car: detail });
-    
-    // req.query.car is to be passed on home.handlebar
-    // display parsed querystring object to the handlebar
-    // car is the keyword (key) to be searched more detail
-    // becomes ==> detail?car=model
-    // car is then an object
+
+// // send content of 'home' (detail page) view using Express
+// app.get('/detail', (req, res) => {
+//     let detail = cars.getCar(req.query.make)
+//     res.render('details', { make: req.query.make, car: detail });
+
+// //     // req.query.car is to be passed on home.handlebar
+// //     // display parsed querystring object to the handlebar
+// //     // car is the keyword (key) to be searched more detail
+// //     // becomes ==> detail?car=model
+// //     // car is then an object
+//  });
+
+
+//send content of 'home' (detail page) view using MongoDB
+app.get('/detail', (req, res, next) => {
+  let searchItem = req.query.make
+  Car.findOne({ "make": searchItem }).lean()
+    .then((cars) => {
+      res.render('details', { cars });
+
+    })
+    .catch(err => next(err));
 });
+
 
 
 // define 404 handler
 app.use((req, res) => {
-    res.type('text/plain');
-    res.status(404);
-    res.send('ERROR: 404 Not found');
+  res.type('text/plain');
+  res.status(404);
+  res.send('ERROR: 404 Not found');
 });
 
 app.listen(app.get('port'), () => {
